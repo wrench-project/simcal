5bea9bdc856e85e530b3a281d803d156b9b10e4e
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-02-22 17:31:08 -1000

    add ordinal param

--- simcal/calibrator_param.py
+++ simcal/calibrator_param.py
@@ -78,8 +78,8 @@ class ExponentialParam(OrderedParam):
         return value
 
     def to_normalized(self, x: float):
-        if self.from_normalize_override:
-            return self.from_normalize_override(self, x)
+        if self.to_normalize_override:
+            return self.to_normalize_override(self, x)
         x_normal = (math.log2(x) - self.start) / (self.end - self.start)
         return x_normal * (self.range_end - self.range_start) + self.range_start
 
@@ -100,8 +100,8 @@ class LinearParam(OrderedParam):  # requires testing
         return value
 
     def to_normalized(self, x: float):
-        if self.from_normalize_override:
-            return self.from_normalize_override(self, x)
+        if self.to_normalize_override:
+            return self.to_normalize_override(self, x)
         x_normal = (x - self.start) / (self.end - self.start)
         return x_normal * (self.range_end - self.range_start) + self.range_start
 
@@ -109,15 +109,24 @@ class LinearParam(OrderedParam):  # requires testing
 class OrdinalParam(OrderedParam):
     def __init__(self, options):
         super().__init__(0, 1)
-        self.option = options
+        self.options = options
 
     def from_normalized(self, x: float):
         if self.from_normalize_override:
             return self.from_normalize_override(self, x)
+        x_normal = (x - self.range_start) / (self.range_end - self.range_start)
+        idx = int(x_normal * len(self.options))
+        value = self.options[idx]
+        if self.formatter:
+            return _FormattedValue(self.formatter, value)
+        return value
 
     def to_normalized(self, x: any):
-        if self.from_normalize_override:
-            return self.from_normalize_override(self, x)
+        if self.to_normalize_override:
+            return self.to_normalize_override(self, x)
+        idx = self.options.index(x)
+        x_normal = idx / len(self.options)
+        return x_normal * (self.range_end - self.range_start) + self.range_start
 
 
 class CategoricalParam(_InternalParam):
--- test/test_calibrator_param.py
+++ test/test_calibrator_param.py
@@ -49,3 +49,42 @@ class TestCalibratorParam(TestCase):
         a = advanced.to_normalized(a)
 
         self.assertAlmostEqual(a, 5, delta=0.01)
+
+    def test_OrdinalParam(self):
+        basic = OrdinalParam(("a", "b", "c", "d", "e"))
+        b = basic.from_normalized(0.5)
+        self.assertEqual(b, "c")
+        b = basic.to_normalized(b)
+        self.assertAlmostEqual(b, 0.4, delta=0.01)
+
+        formatted = OrdinalParam((1, 2, 3, 4, 5, 6, 7, 8)).format("%.1f formatted")
+        f = formatted.from_normalized(0.5)
+        self.assertEqual(str(f), "5.0 formatted")
+        f = formatted.to_normalized(f)
+        self.assertAlmostEqual(f, 0.5, delta=0.01)
+
+        advanced = OrdinalParam(("a", "b", "c", "d", "e"))
+        advanced.range_start = 5
+        advanced.range_end = 10
+        a = advanced.from_normalized(8)
+
+        self.assertEqual(a, "d")
+        a = advanced.to_normalized(a)
+
+        self.assertAlmostEqual(a, 8, delta=0.01)
+
+        lambda_param= OrdinalParam(("a", "b", "c", "d", "e"))
+        lambda_param.from_normalize_override = lambda this, x: this.options[0] if x < 0.5 \
+            else this.options[math.ceil((x - .5) * 5)]
+
+        lambda_param.to_normalize_override = lambda this, x: 0 if x == "a" \
+            else this.options.index(x)/5+.4
+
+        lam = lambda_param.from_normalized(0.2)
+        self.assertEqual(lam, "a")
+        lam = lambda_param.to_normalized(lam)
+        self.assertEqual(lam, 0)
+        lam = lambda_param.from_normalized(0.8)
+        self.assertEqual(lam, "c")
+        lam = lambda_param.to_normalized(lam)
+        print(lam)
