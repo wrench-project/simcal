cc049b7d8253576c308667b16d05f82c0cd56791
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-02-28 17:02:24 -1000

    Update __init__.py

--- simcal/__init__.py
+++ simcal/__init__.py
@@ -1,11 +1,13 @@
 from simcal.simulator import Simulator
 from simcal.calibrators.calibrator import Calibrator
+from simcal.calibrators.grid_calibrator import GridCalibrator
 import simcal.utility_functions as utility_functions
 import inspect
 utilities = inspect.getmembers(utility_functions, inspect.isfunction)
 __all__ = [
     "Simulator",
     "Calibrator",
+    "GridCalibrator",
     "DataLoader",
     "JSONTemplate"
 ]+utilities
