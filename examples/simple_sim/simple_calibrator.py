52bee016895efeec2c10915ed234ae5dcb8dcd57
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-03-07 17:30:24 -1000

    fixed major error

--- examples/simple_sim/simple_calibrator.py
+++ examples/simple_sim/simple_calibrator.py
@@ -34,35 +34,37 @@ class Scenario:
             res.append(simulator((x, unpacked)))
         return res
 
+if __name__ == '__main__':# required if using ThreadPool on Windows or Mac
+    # otherwise a large not useful exception will thrown
 
-# make some fake evaluation scenarios for the example
-evaluation_scenarios = []
-for x in (1.39904, 254441, 5.05656):
-    for y in (1.1558, 3.384, 40395, 7.36):
-        for z in (0.637, 2.281, 3.876, 5.459, 7.038):
-            for w in (0.448, 1.527, 2.587, 3.641, 4.693, 5.743):
-                evaluation_scenarios.append((x, y, z, w))
+    # make some fake evaluation scenarios for the example
+    evaluation_scenarios = []
+    for x in (1.39904, 254441, 5.05656):
+        for y in (1.1558, 3.384, 40395, 7.36):
+            for z in (0.637, 2.281, 3.876, 5.459, 7.038):
+                for w in (0.448, 1.527, 2.587, 3.641, 4.693, 5.743):
+                    evaluation_scenarios.append((x, y, z, w))
 
-# get ground truth data the fake scenarios
-data = []
-for x in evaluation_scenarios:
-    data.append(ground_truth(*x))
+    # get ground truth data the fake scenarios
+    data = []
+    for x in evaluation_scenarios:
+        data.append(ground_truth(*x))
 
-loss = sklearn_mean_squared_error
+    loss = sklearn_mean_squared_error
 
-simulator = ExampleSimulator()
-scenario1 = Scenario(simulator, evaluation_scenarios)
+    simulator = ExampleSimulator()
+    scenario1 = Scenario(simulator, evaluation_scenarios)
 
-# prepare the calibrator and setup the arguments to calibrate with their ranges
-calibrator = sc.calibrators.Grid()  # tbd
+    # prepare the calibrator and setup the arguments to calibrate with their ranges
+    calibrator = sc.calibrators.Grid()  # tbd
 
-calibrator.add_param("a", sc.parameter.Linear(0, 20).format("%.2f"))
-calibrator.add_param("b", sc.parameter.Linear(0, 8).format("%.2f"))
-calibrator.add_param("c", sc.parameter.Linear(0, 10).format("%.2f"))
-calibrator.add_param("d", sc.parameter.Linear(0, 6).format("%.2f"))
+    calibrator.add_param("a", sc.parameter.Linear(0, 20).format("%.2f"))
+    calibrator.add_param("b", sc.parameter.Linear(0, 8).format("%.2f"))
+    calibrator.add_param("c", sc.parameter.Linear(0, 10).format("%.2f"))
+    calibrator.add_param("d", sc.parameter.Linear(0, 6).format("%.2f"))
 
-coordinator = sc.coordinators.ThreadPool(pool_size=8)  # Making a coordinator is optional, and only needed if you wish
-# To run multiple simulations at once, possibly using multiple cpu cores or multiple compute nodes
+    coordinator = sc.coordinators.ThreadPool(pool_size=8)  # Making a coordinator is optional, and only needed if you wish
+    # To run multiple simulations at once, possibly using multiple cpu cores or multiple compute nodes
 
-calibration = calibrator.calibrate(scenario1, loss, data, timeout=60, coordinator=coordinator)
-print(calibration)
+    calibration = calibrator.calibrate(scenario1, loss, data, timeout=60, coordinator=coordinator)
+    print(calibration)
