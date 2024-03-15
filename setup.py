a215374a6442f8ee5b274176554ec3ff0c78b494
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-03-14 14:18:09 -1000

    setup.py

--- setup.py
+++ setup.py
@@ -7,38 +7,32 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+from simcal.version import __version__
 from setuptools import setup, find_packages
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-# Fetch the version
-exec(open('wrench/version.py').read())
-
 setup(
-    name='wrench-api',
+    name='simcal',
     version=str(__version__),
     license='GPLv3',
     author='WRENCH team',
     author_email='support@wrench-project.org',
-    description='A Python API for the WRENCH simulation framework',
+    description='A simulator calibration framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/wrench-project/wrench-api',
+    url='https://github.com/wrench-project/simcal',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'requests'
+        'scikit-learn',
+        'numpy'
     ],
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.12',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
@@ -46,5 +40,5 @@ setup(
         'Topic :: Documentation :: Sphinx',
         'Topic :: System :: Distributed Computing'
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.12'
 )
---
+++ simcal/version.py
@@ -0,0 +1,10 @@
+# -- coding utf-8 --
+#
+# Copyright (c) 2024 The WRENCH Team.
+#
+# This program is free software you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+__version__ = "0.1-dev"
