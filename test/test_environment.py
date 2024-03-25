1b11a2fee7ac1480fe5332ce2059226295b5f705
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-03-25 13:12:56 -1000

    Update test_environment.py

--- test/test_environment.py
+++ test/test_environment.py
@@ -6,12 +6,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, False)  # add assertion here
 
     def test_tempfile(self):
-
+        pass
     def test_2tmpdir(self):
-
+        pass
     def test_2tmpfile(self):
-
+        pass
     def test_2tmpdirWith2tmpfile(self):
-
+        pass
 if __name__ == '__main__':
     unittest.main()
