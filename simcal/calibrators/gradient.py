23d87dcc5cffc33b1cc0f49a06692d3ebf5f2cfa
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-03-28 17:02:12 -1000

    Update gradient.py

--- simcal/calibrators/gradient.py
+++ simcal/calibrators/gradient.py
@@ -36,8 +36,6 @@ class GradientDescent(sc.Base):
         dimensions = len(vector_mapping)
         param_vector = np.empty(dimensions)
 
-
-
         while True:
             for i, key in enumerate(vector_mapping):
                 param_vector[i] = self._ordered_params[key].to_normalized(best[key])
@@ -62,7 +60,7 @@ class GradientDescent(sc.Base):
                 best = self._populate(param_vector, vector_mapping, best_categorical)
             if self.early_reject_loss is not None and best_loss >= self.early_reject_loss:
                 break
-            #print("finding gradient")
+            print("finding gradient")
             loss_at_param = best_c_loss
             # find gradient
             gradient = np.empty(dimensions)
@@ -80,7 +78,7 @@ class GradientDescent(sc.Base):
             # h(xi)=f(xi)+gradient(dot)norm_gradient*scale
 
             # backtracking line search
-            #print("backtracking Line search")
+            print("backtracking Line search")
             grad_norm = normalize([gradient], norm="l2")[
                 0]  # why is scikit normalize so werid?  just take a 1d vector and return a scaler!
             backtrack = learning_rate * 10
@@ -108,7 +106,7 @@ class GradientDescent(sc.Base):
                     break
             previous_loss = best_loss
 
-        return best_loss, best
+        return best, best_loss
 
     def calibrate(self, evaluate_point, early_stopping_loss=None, iterations=None,
                   timelimit=None, coordinator=None):
@@ -116,7 +114,7 @@ class GradientDescent(sc.Base):
             internal = sc.Grid()
         else:
             internal = sc.Random(self.seed)
-        #print("starting new gradient")
+        print("starting new gradient")
         internal._ordered_params = self._ordered_params
         internal._categorical_params = self._categorical_params
 
@@ -124,22 +122,17 @@ class GradientDescent(sc.Base):
             return internal.calibrate(evaluate_point, early_stopping_loss, iterations, timelimit, coordinator)
         else:  # we already have a good calibrator for random points, let it figure out the starts, then route back through us for the descending
             functor = _GradientFunctor(self, evaluate_point)
-            return internal.calibrate(functor, early_stopping_loss, iterations, timelimit, coordinator)
-
+        internal.calibrate(functor, early_stopping_loss, iterations, timelimit, coordinator)
+        return _GradientFunctor.best,_GradientFunctor.best_loss
 
 class _GradientFunctor(object):
     # this lets use use random search.  we make a functor with access the gradient descent, then we let random call the functor, which calls the descent function in gradient descent, which in turn calls the actual functor given to us
     def __init__(self, grad, evaluate_point):
         self.grad = grad
         self.evaluate_point = evaluate_point
+        self.best = None
+        self.best_loss = None
 
     def __call__(self, calibration):
-        return self.grad.descend(self.evaluate_point, calibration)
-
-
-def sampleHyperplane(sample_at, relative_to, slope, vector, stepSize):
-    s = slope
-    for i in range(len(sample_at)):
-        # print(x[i],ref[i],x[i]-ref[i],vector[i]/stepSize,(x[i]-ref[i])*vector[i]/stepSize)
-        s += (sample_at[i] - relative_to[i]) * vector[i] / stepSize
-    return s
+        self.best, self.best_loss = self.grad.descend(self.evaluate_point, calibration)
+        return self.best
