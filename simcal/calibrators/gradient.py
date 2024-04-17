4e901d59e8d3d43bb325ad465f3130ab60c134c1
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-04-16 16:48:50 -1000

    added parameter boundaries to gradient

--- simcal/calibrators/gradient.py
+++ simcal/calibrators/gradient.py
@@ -22,6 +22,15 @@ class GradientDescent(sc.Base):
         args = self._populate(param_vector, vector_mapping, categoricals)
         return evaluate_point(args)
 
+    def _clamp_vector(self, param_vector, vector_mapping):
+        for i in range(len(param_vector)):
+            param = self._get_raw_param(i, vector_mapping)
+            param_vector[i] = sorted((param.range_start, param_vector[i], param.range_end))[1]  #
+        return param_vector
+
+    def _get_raw_param(self, index, vector_mapping):
+        return self._ordered_params[vector_mapping[index]]
+
     def descend(self, evaluate_point, initial_point):
         # TODO (later) gracefully handle early stops
         if not self._categorical_params:
@@ -66,9 +75,16 @@ class GradientDescent(sc.Base):
             gradient = np.empty(dimensions)
             for i in range(dimensions):
                 tmp_vector = param_vector.copy()
-                tmp_vector[i] += self.delta
+                # if we need to calculate a gradient where this goes outise the search space, flip sides and check the other way
+                # but you have to account for the flipped vector when calculating the gradient
+                if self._get_raw_param(i, vector_mapping).range_end < tmp_vector[i] + self.delta:
+                    multiplier = 1
+                else:
+                    multiplier = -1
+
+                tmp_vector[i] += self.delta * multiplier
                 direction_loss = self._evaluate_vector(evaluate_point, tmp_vector, vector_mapping, best_categorical)
-                gradient[i] = (direction_loss - loss_at_param) / learning_rate
+                gradient[i] = (direction_loss - loss_at_param) / self.delta * multiplier
                 if direction_loss < best_loss:
                     best_loss = direction_loss
                     best = self._populate(tmp_vector, vector_mapping, best_categorical)
@@ -79,15 +95,17 @@ class GradientDescent(sc.Base):
 
             # backtracking line search
             # print("backtracking Line search")
-            grad_norm = normalize([gradient], norm="l2")[
-                0]  # why is scikit normalize so werid?  just take a 1d vector and return a scaler!
+            grad_norm = normalize([gradient], norm="l2")[0]
+            # why is scikit normalize so werid?  just take a 1d vector and return a scaler!
             backtrack = learning_rate * 10
             last_check = False
             while True:
                 gradient_step = grad_norm * backtrack
                 backtrack_test = param_vector + gradient_step
+                self._clamp_vector(backtrack_test, vector_mapping)
                 # expected = loss_at_param + gradient.dot(gradient_step)
                 # this simplifies out since we only check if we are going uphill to decide to backtrack
+
                 actual = self._evaluate_vector(evaluate_point, backtrack_test, vector_mapping, best_categorical)
                 if actual < best_loss:
                     best_loss = actual
@@ -123,8 +141,7 @@ class GradientDescent(sc.Base):
             internal._eval = self.descend
         return internal.calibrate(evaluate_point, early_stopping_loss, iterations, timelimit, coordinator)
 
-
-#class _GradientFunctor(object):
+# class _GradientFunctor(object):
 #    # this lets use use random search.  we make a functor with access the gradient descent, then we let random call the functor, which calls the descent function in gradient descent, which in turn calls the actual functor given to us
 #    def __init__(self, grad, evaluate_point):
 #        self.grad = grad
