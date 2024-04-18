e3a99a007b7bd284a328c3a64058c2b16a01db84
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-04-17 21:54:13 -1000

    hard limit

--- examples/simple_sim/simple_calibrator.py
+++ examples/simple_sim/simple_calibrator.py
@@ -72,10 +72,10 @@ calibrator.add_param("b", sc.parameter.Linear(0, 8).format("%.2f"))
 calibrator.add_param("c", sc.parameter.Linear(0, 10).format("%.2f"))
 calibrator.add_param("d", sc.parameter.Linear(0, 6).format("%.2f"))
 
-coordinator = sc.coordinators.ThreadPool(pool_size=8)  # Making a coordinator is optional, and only needed if you
+coordinator = sc.coordinators.ThreadPool(pool_size=4,timelimit=10)  # Making a coordinator is optional, and only needed if you
 # wish to run multiple simulations at once, possibly using multiple cpu cores or multiple compute nodes
 start = time()
-calibration, loss = calibrator.calibrate(scenario1, timelimit=600, coordinator=coordinator)
+calibration, loss = calibrator.calibrate(scenario1, timelimit=60, coordinator=coordinator)
 print("final calibration")
 print(calibration)
 print(loss)
