6ac8e8577c4825d2c5610cbad7236457df709a07
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-12-18 16:22:12 -1000

    Update base.py

--- simcal/calibrators/base.py
+++ simcal/calibrators/base.py
@@ -5,26 +5,26 @@ import simcal.coordinators.base as Coordinator
 import simcal.simulator as Simulator
 from simcal.parameters import Categorical, Value
 from simcal.parameters import Ordered
-
+import time
 
 class Base(object):
     def __init__(self):
         self._ordered_params = {}
         self._categorical_params = {}
-        self.timeline=[] # all best calibrations in order (up to _max_timeline to prevent memory issues)
-        self._max_timeline=100000
-        self.current_best=None
+        self.timeline = []  # all best calibrations in order (up to _max_timeline to prevent memory issues)
+        self._max_timeline = 100000
+        self.current_best = None
 
-    def mark_calibration(self,calibration):
-        time = datetime.now()
+    def mark_calibration(self, calibration):
+        timestamp = int(time.time())
         if len(self.timeline) > self._max_timeline:
             self.timeline.pop(0)
-        self.timeline.append((time,calibration))
-        self.current_best=calibration
+        self.timeline.append((timestamp, calibration))
+        self.current_best = calibration
 
     def calibrate(self, simulator: Simulator, early_stopping_loss: float | int | None = None,
                   iterations: int | None = None, timelimit: float | int | None = None,
-                  coordinator: Coordinator.Base | None = None) -> tuple[dict[str, Value | float | int],float]:
+                  coordinator: Coordinator.Base | None = None) -> tuple[dict[str, Value | float | int], float]:
 
         raise NotImplementedError(f"{self.__class__.__name__} does not define calibrate(self, simulator, "
                                   f"compute_loss, reference_data, iterations=None, timeout=None)")
